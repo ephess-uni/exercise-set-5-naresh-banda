57c989a56edb838a3d969c9e5c4f018de5f25c07
naresh-banda <149258718+naresh-banda@users.noreply.github.com>
2023-12-11 16:23:26 -0600

    Update ex_5_0.py

--- src/ex_5_0.py
+++ src/ex_5_0.py
@@ -2,7 +2,11 @@
 
 
 def line_count(infile):
-    pass
+    count = 0
+    with open(infile, 'r') as f:
+        for line in f:
+            count += 1
+    return count
 
 
 if __name__ == "__main__":
